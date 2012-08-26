c983d96164e08a32fc767bd42c4911d6b38b3edf
Adam Menges <adammenges@gmail.com>
2012-08-26 08:52:03 -0600

    Bug fixes
    Special thanks to Eric Lammerts (lammerts.org) for his help.

--- chrome/content/prowlingFireFox.js
+++ chrome/content/prowlingFireFox.js
@@ -1,3 +1,8 @@
+//
+// Special thanks to Eric Lammerts (lammerts.org) for his help.
+//
+
+
 var prowlingFireFox = function () {
 	var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
 	return {
@@ -5,33 +10,45 @@ var prowlingFireFox = function () {
 			gBrowser.addEventListener("load", function () {
 				var autoRun = prefManager.getCharPref("extensions.prowlingfirefox.autorun");
 				if (!autoRun) {
-					//put alert here
+					// put alert here, if needed.
 				}
 			}, false);
 		},
 			
 		run : function () {
-			        var req = new XMLHttpRequest();
+				var req = new XMLHttpRequest();
+				req.onreadystatechange = function(){
+					if (req.readyState == 1) {
+						var url = window.top.getBrowser().selectedBrowser.contentWindow.location.href;
+						var title = window.top.getBrowser().selectedBrowser.contentDocument.title;
+						var api = prefManager.getCharPref("extensions.prowlingfirefox.api");
 
-			        req.onreadystatechange = function(){
-			                if (req.readyState == 4) {
-			                        if (req.status == 200) {
-			                        }
-			                }
-			        };
-			
-					var url = window.top.getBrowser().selectedBrowser.contentWindow.location.href; //Fix '+' bug
-					var api = prefManager.getCharPref("extensions.prowlingfirefox.api");
+						var params = 'apikey='+ api +
+						     '&application='+ "Send2Prowl" +
+						     '&event='+ escape(title) +
+						     '&url='+ escape(url) +
+						     '&description='+ escape(url);
 
-			        var params = 'apikey='+ api +
-			                     '&application='+ "Send2Prowl" +
-			                     '&event='+ "" +
-			                     '&url='+ url + 
-			                     '&description='+ url;
-
-			        req.open('POST', 'https://prowlapp.com/publicapi/add/?' + params, true);
-			        req.send(null);
+						req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+						req.send(params);
+					}
+					if (req.readyState == 4) {
+						if (req.status != 200) {
+							var errmsg = 'Error sending URL to Prowl: ' + req.status;
+							if(req.responseXML) {
+								var errs = req.responseXML.getElementsByTagName('error');
+								if(errs.length) {
+									errmsg += ' (' + errs[0].textContent + ')';
+								}
+							}
+							alert(errmsg);
+						}
+					}
+				};
+				req.open('POST', 'https://prowlapp.com/publicapi/add', true);
 		}
 	};
 }();
 window.addEventListener("load", prowlingfirefox.init, false);
+
+
